ffeab767b713699a130d42977b7abd9e57c2e91b
tharkusha <vladreviakin@ukr.net>
2024-03-16 21:07:45 +0000

    1+

--- js/task-3.js
+++ js/task-3.js
@@ -9,3 +9,11 @@ const profile = {
       return `${username} has ${playTime} active hours!`
   }
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
